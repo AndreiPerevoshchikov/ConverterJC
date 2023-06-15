40756aa69a4e3aefff134505c4f901b1e7559764
AndreiPerevoshchikov <113555470+AndreiPerevoshchikov@users.noreply.github.com>
2023-06-15 23:00:54 +0300

    Update main.py
    Добавил функцию для открытия csv файла в excel, обновил дизайн приложения

--- main.py
+++ main.py
@@ -4,14 +4,18 @@ from customtkinter import filedialog as fd
 import pathlib
 from PIL import Image, ImageTk
 from itertools import count, cycle
+import os
+import pandas as pd
 
 
 class ImageLabel(CTkLabel):
     def load(self, im):
         """
         Загружает и отображает анимированное изображение в виджете Label.
+
         Параметры:
             im (str): Путь к изображению.
+
         """
         im = Image.open(im)
         frames = []
@@ -76,10 +80,30 @@ def convert():
     CTkLabel(root, text='Конвертация завершена', font=('Arial', 15)).pack(pady=10)
 
 
+def open_csv_file():
+    """
+    Открывает выбранный CSV файл в Excel.
+    """
+    file_path = filedialog.askopenfilename(filetypes=[("CSV Files", "*.csv")])
+    if file_path:
+        df = pd.read_csv(file_path)
+        excel_file_path = os.path.splitext(file_path)[0] + ".xlsx"
+        df.to_excel(excel_file_path, index=False)
+        os.startfile(excel_file_path)  # Открываем XLSX файл в Excel
+
+
 if __name__ == '__main__':
+    # Инициализация пользовательской библиотеки
+    set_appearance_mode("dark")
+    set_default_color_theme("dark-blue")
+
     root = CTk()
+    lb = ImageLabel(root, text="")
+    lb.pack()
+    lb.load('test.gif')
+
     root.title('Конвертер json в csv')
-    root.geometry('800x400+400+400')
+    root.geometry('600x600+400+400')
     root.resizable(width=False, height=False)
 
     CTkButton(root, text='Выбрать json файл', font=('Arial', 15), command=callback).pack(pady=10)
@@ -91,8 +115,7 @@ if __name__ == '__main__':
     ePath.pack(pady=10)
 
     btnConvert = CTkButton(root, text='Конвертировать', font=('Arial', 15), command=convert).pack(pady=10)
+    open_button = CTkButton(root, text="Открыть CSV файл", command=open_csv_file)
+    open_button.pack()
 
-    lb = ImageLabel(root)
-    lb.pack()
-    lb.load('test.gif')
     root.mainloop()
